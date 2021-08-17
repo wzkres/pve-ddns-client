41bb6f48ccbcc1990d2addf2bbb7d1dc8891b397
wzkres <kres@k-res.net>
2021-08-17 22:37:54 +0800

    Add openssl, curl deps
    And simple shell script to build 3rdparty libraries
    
    Signed-off-by: wzkres <kres@k-res.net>

---
+++ 3rdparty/build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+script_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echo "Removing prebuilt dir..."
+rm -rf ${script_path}/prebuilt
+echo "Creating prebuilt dir..."
+mkdir -p ${script_path}/prebuilt
+
+echo "Building openssl..."
+cd ${script_path}/openssl
+./config no-shared --prefix=${script_path}/prebuilt --openssldir=${script_path}/prebuilt
+make -j4
+make install
+make clean
+echo "openssl built!"
+
+echo "Building curl..."
+cd ${script_path}/curl
+rm -rf cmake_build
+mkdir cmake_build
+cd cmake_build
+cmake -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=${script_path}/prebuilt ../
+make -j4
+make install
+make clean
+echo "curl built!"
+
+echo "Building glog..."
+cd ${script_path}/glog
+rm -rf cmake_build
+mkdir cmake_build
+cd cmake_build
+cmake -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=${script_path}/prebuilt ../
+make -j4
+make install
+make clean
+echo "glog built!"
+
+echo "Building yaml-cpp..."
+cd ${script_path}/yaml-cpp
+rm -rf cmake_build
+mkdir cmake_build
+cd cmake_build
+cmake -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=${script_path}/prebuilt ../
+make -j4
+make install
+make clean
+echo "yaml-cpp built!"
