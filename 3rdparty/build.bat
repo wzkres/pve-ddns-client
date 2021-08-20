70cf59e6b047bee7dbeec2a5b75370551b8104d5
wzkres <kres@k-res.net>
2021-08-20 23:39:59 +0800

    Add windows 3rdparty libs build.bat
    For windows development purpose

---
+++ 3rdparty/build.bat
@@ -0,0 +1,68 @@
+@echo off
+
+echo Remove prebuilt folder...
+rd /S /Q prebuilt
+echo Create prebuild folder...
+md prebuilt
+
+goto comment
+echo Building openssl...
+cd openssl-cmake
+rd /S /Q cmake_build
+md cmake_build
+cd cmake_build
+cmake -G Ninja -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../../prebuilt ../
+ninja
+ninja install
+cd ..
+rd /S /Q cmake_build
+cd ..
+echo openssl built!
+:comment
+
+echo Building curl...
+cd curl
+rd /S /Q cmake_build
+md cmake_build
+cd cmake_build
+cmake -G Ninja -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Debug -DCMAKE_USE_SCHANNEL=YES ^
+  -DCMAKE_INSTALL_PREFIX:PATH=../../prebuilt ^
+  ../
+ninja
+ninja install
+cd ..
+rd /S /Q cmake_build
+cd ..
+echo curl built!
+
+echo Building glog...
+cd glog
+rd /S /Q cmake_build
+md cmake_build
+cd cmake_build
+cmake -G Ninja -DWITH_GFLAGS=NO -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Debug ^
+  -DCMAKE_INSTALL_PREFIX:PATH=../../prebuilt ^
+  ../
+ninja
+ninja install
+cd ..
+rd /S /Q cmake_build
+cd ..
+echo glog built!
+
+echo Building yaml-cpp...
+cd yaml-cpp
+rd /S /Q cmake_build
+md cmake_build
+cd cmake_build
+cmake -G Ninja -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Debug ^
+  -DCMAKE_INSTALL_PREFIX:PATH=../../prebuilt ^
+  ../
+ninja
+ninja install
+cd ..
+rd /S /Q cmake_build
+cd ..
+echo yaml-cpp built!
+
+echo All 3rdparty libs built!
