4a21d563d69a310141a242cd76261a4dea789922
K-Res <kres@k-res.net>
2021-09-01 17:19:03 +0800

    Remove ninja build

--- 3rdparty/build.bat
+++ 3rdparty/build.bat
@@ -9,11 +9,9 @@ goto comment
 echo Building openssl...
 cd openssl-cmake
 rd /S /Q cmake_build
-md cmake_build
-cd cmake_build
-cmake -G Ninja -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../prebuilt ../
-ninja
-ninja install
+cmake -B ./cmake_build -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../prebuilt
+cmake --build ./cmake_build --config Release
+cmake --install ./cmake_build
 cd ..
 rd /S /Q cmake_build
 cd ..
@@ -23,13 +21,10 @@ echo openssl built!
 echo Building curl...
 cd curl
 rd /S /Q cmake_build
-md cmake_build
-cd cmake_build
-cmake -G Ninja -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release -DCMAKE_USE_SCHANNEL=YES ^
-  -DCMAKE_INSTALL_PREFIX:PATH=../../prebuilt ^
-  ../
-ninja
-ninja install
+cmake -B ./cmake_build -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release -DCMAKE_USE_SCHANNEL=YES ^
+  -DCMAKE_INSTALL_PREFIX:PATH=../../prebuilt
+cmake --build ./cmake_build --config Release
+cmake --install ./cmake_build
 cd ..
 rd /S /Q cmake_build
 cd ..
@@ -38,13 +33,10 @@ echo curl built!
 echo Building glog...
 cd glog
 rd /S /Q cmake_build
-md cmake_build
-cd cmake_build
-cmake -G Ninja -DWITH_GFLAGS=NO -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release ^
-  -DCMAKE_INSTALL_PREFIX:PATH=../../prebuilt ^
-  ../
-ninja
-ninja install
+cmake -B ./cmake_build -DWITH_GFLAGS=NO -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_INSTALL_PREFIX:PATH=../../prebuilt
+cmake --build ./cmake_build --config Release
+cmake --install ./cmake_build
 cd ..
 rd /S /Q cmake_build
 cd ..
@@ -53,13 +45,10 @@ echo glog built!
 echo Building yaml-cpp...
 cd yaml-cpp
 rd /S /Q cmake_build
-md cmake_build
-cd cmake_build
-cmake -G Ninja -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release ^
-  -DCMAKE_INSTALL_PREFIX:PATH=../../prebuilt ^
-  ../
-ninja
-ninja install
+cmake -B ./cmake_build -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_INSTALL_PREFIX:PATH=../../prebuilt
+cmake --build ./cmake_build --config Release
+cmake --install ./cmake_build
 cd ..
 rd /S /Q cmake_build
 cd ..
