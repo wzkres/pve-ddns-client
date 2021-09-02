aa65e9d9492d34ee43df9b55f147fe157b413a7e
K-Res <kres@k-res.net>
2021-09-02 18:40:11 +0800

    Disable libunwind when building glog

--- 3rdparty/build.sh
+++ 3rdparty/build.sh
@@ -32,7 +32,7 @@ cd ${script_path}/glog
 rm -rf cmake_build
 mkdir cmake_build
 cd cmake_build
-cmake -DWITH_GFLAGS=NO -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=${script_path}/prebuilt ../
+cmake -DWITH_GFLAGS=NO -DWITH_UNWIND=NO -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=${script_path}/prebuilt ../
 make -j4
 make install
 make clean
