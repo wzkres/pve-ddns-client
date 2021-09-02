7e44b777c305af68d7b073a582c750b53d55ba73
K-Res <kres@k-res.net>
2021-09-02 17:59:27 +0800

    Disable libssh2 when building curl

--- 3rdparty/build.sh
+++ 3rdparty/build.sh
@@ -19,7 +19,7 @@ cd ${script_path}/curl
 rm -rf cmake_build
 mkdir cmake_build
 cd cmake_build
-cmake -DCURL_DISABLE_LDAP=YES -DCURL_DISABLE_LDAPS=YES -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=${script_path}/prebuilt ../
+cmake -DCURL_DISABLE_LDAP=YES -DCURL_DISABLE_LDAPS=YES -DCMAKE_USE_LIBSSH2=NO -DBUILD_SHARED_LIBS=NO -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=${script_path}/prebuilt ../
 make -j4
 make install
 make clean
