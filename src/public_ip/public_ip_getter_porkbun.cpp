ae69abe7711fe000cf363b226cf1dbe96d43db08
K-Res <kres@k-res.net>
2026-06-15 21:22:49 +0800

    Update API_HOST of porkbun

--- src/public_ip/public_ip_getter_porkbun.cpp
+++ src/public_ip/public_ip_getter_porkbun.cpp
@@ -8,7 +8,7 @@
 #include "../utils.h"
 #include "../config.h"
 
-static const char * API_HOST = "https://porkbun.com/api/json/v3/";
+static const char * API_HOST = "https://api.porkbun.com/api/json/v3/";
 static const char * API_HOST_V4 = "https://api-ipv4.porkbun.com/api/json/v3/";
 static const char * API_PING = "ping";
 
