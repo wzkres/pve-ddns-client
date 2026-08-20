bd125a7a2cc02927490a610ac1421948d68fbc91
K-Res <kres@k-res.net>
2026-08-20 10:01:41 +0800

    Bump CURL_SSLVERSION_TLSv1_2 to CURL_SSLVERSION_TLSv1_3

--- src/utils.cpp
+++ src/utils.cpp
@@ -371,7 +371,7 @@ bool http_req(const std::string & url, const std::string & req_data, long timeou
         return false;
     }
 //    curl_read_userdata read_userdata = { req_data, 0 };
-    curl_easy_setopt(curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+    curl_easy_setopt(curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_3);
     curl_easy_setopt(curl, CURLoption::CURLOPT_URL, url.c_str());
     curl_easy_setopt(curl, CURLoption::CURLOPT_TIMEOUT_MS, timeout_ms);
     if (!req_data.empty())
